d35059d8defe5c1a5d6dfd58b85bc565df7ff449
Yann Lossouarn <yann@lossouarn.net>
2026-04-09 23:02:10 +0200

    Allow preventing node respawn through environment variable

--- docker/openmower_upstream/scripts/start_mowgli.sh
+++ docker/openmower_upstream/scripts/start_mowgli.sh
@@ -7,9 +7,11 @@ export MOWER="${OM_MOWER:-CUSTOM}"
 export ESC_TYPE="${OM_MOWER_ESC_TYPE:-xesc_mini}"
 export HARDWARE_PLATFORM="${OM_HARDWARE_VERSION:-1}"
 export PARAMS_PATH="${OM_PARAMS_PATH:-/config}"
+export OM_DEVELOPMENT_PREVENT_NODE_RESPAWN="${OM_DEVELOPMENT_PREVENT_NODE_RESPAWN:-false}"
+echo "OM_DEVELOPMENT_PREVENT_NODE_RESPAWN: ${OM_DEVELOPMENT_PREVENT_NODE_RESPAWN}"
 
 # Use legacy config mode: parameters are read from OM_* env vars (set in mower_config.sh)
 # rather than from yaml files
 export OM_LEGACY_CONFIG_MODE="True"
 
-roslaunch --wait open_mower open_mower.launch
+roslaunch --wait -v open_mower open_mower.launch
